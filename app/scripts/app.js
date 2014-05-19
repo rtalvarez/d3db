4c8a81733a520d8bcbdef9b4e18e320495a6f015
Roberto Alvarez <robertoatc02@gmail.com>
2014-05-19 15:27:35 -0700

    Implements basic firebase factory

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,41 @@
 'use strict';
 
-angular.module('d3dbApp', ['firebase']);
+angular.module('d3dbApp', ['firebase'])
 
-angular.module('d3dbApp')
-  .controller('MainCtrl', function ($scope, $firebase) {
+// angular.module('d3dbApp')
+//   .controller('MainCtrl', function ($scope, $firebase) {
 
-    var data = new Firebase("https://incandescent-fire-8620.firebaseio.com");
+//     var data = new Firebase("https://incandescent-fire-8620.firebaseio.com");
 
-    $scope.data = $firebase(data);
-
+//     $scope.data = $firebase(data);
 
    
-  });
+//   });
+
+.factory('graphData', ['$firebase', function($firebase){
+  var ref = new Firebase('https://incandescent-fire-8620.firebaseio.com');
+  var times = [];
+  var values = [];
+  ref.on('child_added', function(snapshot){
+    
+    var read = snapshot.val().split('@');
+    times.push(read[0]);
+    values.push(read[1]);
+
+    console.log(snapshot.val());
+  });  
+  return $firebase(ref);
+}])
+
+// .factory('')
+
+.controller('MainCtrl', ['$scope', 'graphData', function($scope, graphData){
+  console.log(graphData);
+  window.troll = graphData;
+  $scope.data = graphData;
+
+  console.log(times)
+
+  // graphData.$bind($scope, 'text');
+
+}]);
