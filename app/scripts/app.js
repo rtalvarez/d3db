60ff48f83903fd408a3c5e431d49314586b23bea
Roberto Alvarez <robertoatc02@gmail.com>
2014-05-19 15:48:32 -0700

    Displays & saves data to arrays

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,40 +2,27 @@
 
 angular.module('d3dbApp', ['firebase'])
 
-// angular.module('d3dbApp')
-//   .controller('MainCtrl', function ($scope, $firebase) {
+.controller('MainCtrl', ['$scope', '$firebase', function($scope, $firebase){
 
-//     var data = new Firebase("https://incandescent-fire-8620.firebaseio.com");
-
-//     $scope.data = $firebase(data);
-
-   
-//   });
-
-.factory('graphData', ['$firebase', function($firebase){
   var ref = new Firebase('https://incandescent-fire-8620.firebaseio.com');
+
   var times = [];
   var values = [];
+
   ref.on('child_added', function(snapshot){
-    
     var read = snapshot.val().split('@');
     times.push(read[0]);
     values.push(read[1]);
-
     console.log(snapshot.val());
-  });  
-  return $firebase(ref);
-}])
+  });
 
-// .factory('')
+  ref = $firebase(ref);
 
-.controller('MainCtrl', ['$scope', 'graphData', function($scope, graphData){
-  console.log(graphData);
-  window.troll = graphData;
-  $scope.data = graphData;
-
-  console.log(times)
-
-  // graphData.$bind($scope, 'text');
+  $scope.data = ref;
+  ref.$bind($scope, 'text');
 
 }]);
+
+
+
+
